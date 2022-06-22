2d134eb3f6101b893af2d5eae7948e05addf56e9
Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>
2022-06-22 12:15:31 -0700

    feat: X11 sysroot (#49)
    Signed-off-by: Thulio Ferraz Assis <3149049+f0rmiga@users.noreply.github.com>

--- sysroot/README.md
+++ sysroot/README.md
@@ -8,14 +8,22 @@ aarch64 (aka arm64 and armv8).
 
 ## Building the sysroots
 
-Use the `build_sysroot.sh` script to build the sysroots using Docker. The current restriction is
+Use the `build.sh` script to build the sysroots using Docker. The current restriction is
 that the container must run in x86_64. The sysroots for other architectures are built using
 cross-compilation from x86_64.
 
 ### Using the build script
 
 ```shell
-./sysroot/build_sysroot.sh x86_64 sysroot-x86_64.tar.xz
-./sysroot/build_sysroot.sh armv7 sysroot-armv7.tar.xz
-./sysroot/build_sysroot.sh aarch64 sysroot-aarch64.tar.xz
+./sysroot/build.sh x86_64 sysroot-x86_64.tar.xz base
+./sysroot/build.sh armv7 sysroot-armv7.tar.xz base
+./sysroot/build.sh aarch64 sysroot-aarch64.tar.xz base
+```
+
+### Variants
+
+If you want to build a sysroot containing extra libraries, you can build a variant. E.g. the X11:
+
+```shell
+./sysroot/build.sh x86_64 sysroot-X11-x86_64.tar.xz X11
 ```
--- sysroot/build.sh
+++ sysroot/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 readonly arch=$1
-readonly output=$2
+readonly output_dir=$2
+readonly variant=$3
 
 set -o errexit -o nounset -o pipefail
 
@@ -10,19 +11,27 @@ if [ -z "${arch}" ]; then
     exit 1
 fi
 
-if [ -z "${output}" ]; then
-    >&2 echo "ERROR: the second argument of the script must be the output file path."
+if [ -z "${output_dir}" ]; then
+    >&2 echo "ERROR: the second argument of the script must be the output directory."
+    exit 1
+fi
+
+if [ -z "${variant}" ]; then
+    >&2 echo "ERROR: the third argument of the script must be the variant (base, X11)."
     exit 1
 fi
 
 echo "INFO: building sysroot inside container..."
 
 sysroot_dir="$(git rev-parse --show-toplevel)/sysroot"
+output=$(realpath "${output_dir}/sysroot-${variant}-${arch}.tar.xz")
+image_tag=$(tr '[:upper:]' '[:lower:]' <<<"sysroot-${variant}-${arch}")
 
 (cd "${sysroot_dir}"; \
     docker build \
         --build-arg ARCH="${arch}" \
-        --tag "sysroot-${arch}" \
+        --tag "${image_tag}" \
+        --target "sysroot_${variant}" \
         .)
 
 echo "INFO: exporting sysroot to '${output}'..."
@@ -33,7 +42,7 @@ function remove_tmpdir {
 }
 trap remove_tmpdir EXIT
 
-container_id="$(docker create "sysroot-${arch}")"
+container_id="$(docker create "${image_tag}")"
 function remove_container {
     docker rm "${container_id}"
     remove_tmpdir
